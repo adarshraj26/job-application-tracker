35b357e9a4b78006edeecb263f2ce03f4282a6f0
goDFatHeR26 <ad26081309@gmail.com>
2025-08-02 00:38:14 +0530

    Test: Ultra-simple React render to debug blank screen

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div style={{ padding: '20px', textAlign: 'center' }}>
+    <h1>Hello World!</h1>
+    <p>If you see this, React is working!</p>
+  </div>
 )
